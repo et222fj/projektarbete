22721a67623ae39293acc9677b3f9b8722e04060
OpenShift guest <et222fj@student.lnu.se>
2014-02-02 15:08:04 -0500

    klar med allt

---
+++ script/map.js
@@ -0,0 +1,13 @@
+ function initialize() {
+        var map_canvas = document.getElementById('map_canvas');
+        var map_options = {
+          center: new google.maps.LatLng(57.29095, 13.52245),
+          zoom: 8,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        }
+        var map = new google.maps.Map(map_canvas, map_options)
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
+      
+     
+
